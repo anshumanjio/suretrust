cdcaf772f0617ba52778feb07164b580680b9231
Anshuman <anshumanjio75@gmail.com>
2022-05-28 19:58:29 +0000

    Add heroku config and files

--- hello_heroku/lib/hello_heroku_web/endpoint.ex
+++ hello_heroku/lib/hello_heroku_web/endpoint.ex
@@ -11,7 +11,7 @@ defmodule HelloHerokuWeb.Endpoint do
   ]
 
   socket "/socket", HelloHerokuWeb.UserSocket,
-    websocket: true,
+    websocket: [timeout: 45_000],
     longpoll: false
 
   socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
